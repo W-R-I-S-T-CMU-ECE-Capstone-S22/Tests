1e7f8394dc6a5b0863db88216fb3d650d2d5c88e
Edward <lued2018@gmail.com>
2022-03-09 23:23:15 -0500

    initial commit

---
+++ recv_data.py
@@ -0,0 +1,39 @@
+import time
+import random
+import paho.mqtt.client as mqtt
+
+TOPIC = "wrist/data/sensors"
+
+NUM_SENSORS = 10
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+    client.subscribe(TOPIC)
+
+def on_disconnect(client, userdata, rc):
+    print("Disconnected with result code " + str(rc))
+
+    client.loop_stop()
+
+def on_message(client, userdata, msg):
+    data = list(msg.payload)
+
+    timestamp = int.from_bytes(bytes(data[:-NUM_SENSORS]), "little")
+    curr_time = time.time()
+    elapsed = curr_time - timestamp
+
+    sensor_data = data[-NUM_SENSORS:]
+    print(elapsed, ":\t", sensor_data)
+
+client = mqtt.Client("client" + str(random.randrange(100000, 999999)), clean_session=True)
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_message = on_message
+
+client.connect("mqtt.eclipseprojects.io", 1883, 60)
+
+client.loop_start()
+
+while (1):
+    pass
